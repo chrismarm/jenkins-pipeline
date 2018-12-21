4a379fb887c8c52da27bac358e65c73602dafa9d
chrismarm <chrismarm.development@gmail.com>
2018-12-21 10:19:24 +0100

    WIP: Errors

--- withBash/pipeline/jenkins/build/mvn.sh
+++ withBash/pipeline/jenkins/build/mvn.sh
@@ -4,5 +4,5 @@ echo "****************"
 echo "* Building jar!*"
 echo "****************"
 
-PROJ=/var/jenkins_home/workspace/pipeline-maven/withBash/pipeline
+PROJ=/home/chris/Development/Jenkins/jenkins-pipeline/withBash/pipeline
 docker run --rm -v /root/.m2:/root/.m2 -v $PROJ/java-app:/app -w /app maven:3-alpine "$@"
