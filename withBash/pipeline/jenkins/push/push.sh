22051a05bcce07baaf3dbcd48940afd5f2897576
Christian Laptop <chrismarm.development@gmail.com>
2019-01-07 15:11:40 +0100

    push WIP

--- withBash/pipeline/jenkins/push/push.sh
+++ withBash/pipeline/jenkins/push/push.sh
@@ -7,8 +7,8 @@ echo "########################"
 IMAGE="app"
 
 echo "*** Logging in ***"
-docker login -u chrismarm -p development85
+docker login -u chrismarm -p development85 docker.io
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG $IMAGE:$BUILD_TAG
+docker tag $IMAGE:$BUILD_TAG chrismarm/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
-docker push $IMAGE:$BUILD_TAG
+docker push chrismarm/$IMAGE:$BUILD_TAG
