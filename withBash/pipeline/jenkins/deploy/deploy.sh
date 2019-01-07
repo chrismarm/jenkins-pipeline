71cec7c6f917712b930a41dcae92ae332978eec3
Christian Laptop <chrismarm.development@gmail.com>
2019-01-07 16:18:57 +0100

    Push and deploy steps

--- withBash/pipeline/jenkins/deploy/deploy.sh
+++ withBash/pipeline/jenkins/deploy/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Generamos
+# Store in a file all the needed data to be used in the production server to pull the image and run the container with the app
 echo app > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
-echo $PASS >> /tmp/.auth
+echo $PASS_USR >> /tmp/.auth
+echo $PASS_PWD >> /tmp/.auth
 
 
-# Transifere el archivo
-scp   -i /opt/aws_linuxfacilito.pem /tmp/.auth  centos@linuxfacilito.online:/tmp/.auth
-scp -i /opt/aws_linuxfacilito.pem ./jenkins/deploy/publish centos@linuxfacilito.online:/tmp/publish
-ssh -i /opt/aws_linuxfacilito.pem  centos@linuxfacilito.online /tmp/publish
+# We transfer the file with the info, the script to login and run the docker-compose and the docker-compose itself
+scp -i withBash/production-server/jenkins-access /tmp/.auth  production@production-server:/tmp/.auth
+scp -i withBash/production-server/jenkins-access withBash/pipeline/jenkins/deploy/publish production@production-server:/tmp/publish
+scp -i withBash/production-server/jenkins-access withBash/pipeline/jenkins/deploy/docker-compose.yml production@production-server:/home/production/docker-compose-yml
+ssh -i withBash/production-server/jenkins-access production@production-server /tmp/publish
