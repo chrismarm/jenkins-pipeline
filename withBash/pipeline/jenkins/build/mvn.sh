142d1c9dbd4ccd36186943806788f3cddbbeae4c
Christian Laptop <chrismarm.development@gmail.com>
2019-01-07 13:50:23 +0100

    WIP

--- withBash/pipeline/jenkins/build/mvn.sh
+++ withBash/pipeline/jenkins/build/mvn.sh
@@ -5,4 +5,5 @@ echo "* Building jar!*"
 echo "****************"
 
 PROJ=/home/chris/Development/Jenkins/jenkins-pipeline/withBash/jenkins_home/workspace/pipeline-docker/withBash/pipeline
-docker run --rm -v /root/.m2:/root/.m2 -v $PROJ/java-app:/app -w /app maven:3-alpine "$@"
+echo $PWD
+docker run --rm -v /root/.m2:/root/.m2 -v $PWD/java-app:/app -w /app maven:3-alpine "$@"
